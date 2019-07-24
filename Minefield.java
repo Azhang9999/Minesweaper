1e1628b709e09f85cb1d66f71d44320c0f2e9af9
Ella <schwarzem@sbcglobal.net>
2019-07-24 16:50:47 -0700

    comments added

---
+++ Minefield.java
@@ -0,0 +1,4 @@
+// i guess i can write code or whatever
+// board generator
+// note that when playing, the first spot is never a mine
+// randomly generate a specific number of coordinates for mines
